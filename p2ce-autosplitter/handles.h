1cc98b298b1d4f5aaa55548f56fdb28c1ef40c5b
Vidzhet <pochta@idi.nah>
2024-11-27 04:00:07 +0200

    pre-release big update

---
+++ p2ce-autosplitter/handles.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "includes.h"
+
+// some useful global pointers
+extern uintptr_t engineBaseAddress;
+extern uintptr_t materialsystemBaseAddress;
+extern uintptr_t clientBaseAddress;
+extern uintptr_t panoramaBaseAddress;
+extern uintptr_t gameoverlayrenderer64BaseAddress;
+extern uintptr_t consolelogAddress;
